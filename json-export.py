8e91c67d3e5a4dd5a4559784ee64a799f0a8bf64
Charlie B <cbutcosk@mbp-022084.local>
2017-04-14 12:22:32 -0400

    Template updates + misc

--- json-export.py
+++ json-export.py
@@ -9,18 +9,31 @@ import HTMLParser
 
 #-1: Parse command line args
 '''
-parser = argparse.ArgumentParser()
-parser.add_argument("-u", "--url", required = True, help = "URL of the embark server (eg, http://embark.colby.edu)")
 parser.add_argument("-l","--layout", required = True, help = "Template layout name (eg, ccma_objects)")
 parser.add_argument("-a","--all", required = False, help = "Get all objects")
 parser.add_argument("-t","--type", required = True, help="Embark record type (ie, objects_1, artist_maker, etc)")
-args = vars(parser.parse_args())
 '''
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", "--url", help = "URL of the embark server (default: http://embark.colby.edu)", default = 'http://embark.colby.edu')
+args = vars(parser.parse_args())
+
+# FIXME: Check if there is an "url" argument and use it
+endpoint = args['url']
+path = '/results.html'
+url = endpoint + path
+objects_args = {"layout" : "ccma_objects" , "format" : "shtml", "maximumRecords":"-1", "recordType":"objects_1", "query":"_ID>1"}
+artists_args = {"layout" : "ccma_artists" , "format" : "shtml", "maximumRecords":"-1", "recordType":"artist_maker", "query":"_ID>1"}
+exhibs_args = {"layout" : "ccma_exhibs" , "format" : "shtml", "maximumRecords":"-1", "recordType":"objects_1", "query":"_ID>1"}
+
+# FIXME: Will need to make separate objects, artists, exhibs urls for the requests
+
+
+
 # 0: Get objects, artists, and exhibitions from the embark webkiosk
-objects_response = requests.get('http://embark.colby.edu/results.html?layout=ccma_objects&format=shtml&maximumrecords=-1&recordType=objects_1&query=_ID>1')
-artists_response = requests.get('http://embark.colby.edu/results.html?layout=ccma_artists&format=shtml&maximumrecords=-1&recordType=artist_maker&query=_ID>1')
-exhibs_response =  requests.get('http://embark.colby.edu/results.html?layout=ccma_exhibs&format=shtml&maximumrecords=-1&recordType=objects_1&query=_ID>1')
+objects_response = requests.get(url,objects_args) #requests.get('http://embark.colby.edu/results.html?layout=ccma_objects&format=shtml&maximumrecords=-1&recordType=objects_1&query=_ID>1')
+artists_response = requests.get(url,artists_args) #requests.get('http://embark.colby.edu/results.html?layout=ccma_artists&format=shtml&maximumrecords=-1&recordType=artist_maker&query=_ID>1')
+exhibs_response =  requests.get(url,exhibs_args) #requests.get('http://embark.colby.edu/results.html?layout=ccma_exhibs&format=shtml&maximumrecords=-1&recordType=objects_1&query=_ID>1')
 
 if objects_response.status_code == 200:
 	print "Got objects and exhibitions successfully."
@@ -56,10 +69,10 @@ exhibs = ''.join( exhibs.rsplit(',',1) )
 regex = re.compile("}[ \t]+,[ \t]+\]", re.MULTILINE)
 objects = regex.sub('} ]',objects)
 
-# 3: Strip ^M chars out (necessary? may have just been curl being prickish)
-objects = objects.replace('\r', '')
-artists = artists.replace('\r','')
-exhibs = exhibs.replace('\r','')
+# 3: Strip out newlines and carriage returns 
+objects = objects.replace('\r', '').replace('\n','')
+artists = artists.replace('\r','').replace('\n','')
+exhibs = exhibs.replace('\r','').replace('\n','')
 
 # =======
 # 3a: Go through each line and make sure it ends with '{' , '}' , '[' , ']' , '"', or ','.
@@ -79,23 +92,39 @@ objects_dict = json.loads(objects)
 artists_dict = json.loads(artists)
 exhibs_dict = json.loads(exhibs)
 				
-# 5(-a): Unescape fields in all objects and fix image links in objects
+# 5: Unescape fields in all objects and fix image links in objects
 # FIXME: This should be done in template w/ 4DHTML/4DVAR tags
 parser = HTMLParser.HTMLParser()
 for obj in objects_dict['objects']:
 	
+	# 5a (FIXME in embark): Escape all chars and strip whitespace
 	for key,value in obj.iteritems():
 		if isinstance(value, basestring):
-			obj[key] = parser.unescape(value)
+			obj[key] = parser.unescape(value).strip()
 
+	# 5b: If it's an IIIF image strip the '_cd.jpg' bits (FIXME: catching _cd.JPEG/false PMs/etc)
 	for image in obj['Images']:
 		path = image.get('ImagePath')
 		iiif = image.get('IIIF_URL')
 		
 		if path and iiif:
 			print "Fixing image %s" % path
-			image['ImagePath'] = path.replace("_cd.jpg","").replace("http://iiif.museum.colby.edu/","https://iiif.museum.colby.edu/image/")
-			image['IIIF_URL'] = iiif.replace("_cd.jpg","").replace("http://","https://")
+			image['ImagePath'] = path.replace("_cd.jpg","")
+			image['IIIF_URL'] = iiif.replace("_cd.jpg","")
+
+	# 5b: (FIXME w/ curator/registrar): strip '?' from material and support field
+	if "(?)" in obj['Medium']:
+		print "WARNING: 'Medium' for ID %s AccNo %s has '(?)'" % (obj["embark_ID"],obj["Disp_Access_No"])
+		obj['Medium'] = obj['Medium'].replace("(?)","")
+
+	if "?" in obj['Medium']:
+		print "WARNING: 'Medium' for ID %s AccNo %s has '?'" % (obj["embark_ID"],obj["Disp_Access_No"])
+		obj['Medium'] = obj['Medium'].replace("?","")
+
+	if "?" in obj['Support']:
+		print "WARNING: 'Support' for ID %s AccNo %s has '?'" % (obj["embark_ID"],obj["Disp_Access_No"])
+		obj['Support'] = obj['Support'].replace("?","")
+
 
 for artist in artists_dict['artists']:
 	for key,value in artist.iteritems():
